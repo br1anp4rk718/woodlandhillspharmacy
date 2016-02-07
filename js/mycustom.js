6cc459321a35897e1fef4349493deca7338e5caa
br1anp4rk <br1anp4rk718@gmail.com>
2016-02-06 21:36:25 -0800

    for shared view

---
+++ js/mycustom.js
@@ -0,0 +1,12 @@
+$('#inline-popups, #inline-popups-1, #inline-popups-2, #inline-popups-3').magnificPopup({
+  delegate: 'a',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
+
