5435b1c0bfa60893bfeebdc7f456631c79955c7b
br1anp4rk <br1anp4rk718@gmail.com>
2016-02-16 17:25:17 -0800

    basic navi structure done

--- js/mycustom.js
+++ js/mycustom.js
@@ -1,17 +1,49 @@
-$('#inline-popups, #inline-popups-1, #inline-popups-2, #inline-popups-3, #inline-popups-4, #inline-popups-5, #inline-popups-6, #inline-popups-7, #inline-popups-8, #inline-popups-9, #inline-popups-10, #inline-popups-11, #inline-popups-12, #inline-popups-13,#inline-popups-14,#inline-popups-15,#inline-popups-16,#inline-popups-17,#inline-popups-18,#inline-popups-19,#inline-popups-20,#inline-popups-21,#inline-popups-22,#inline-popups-23,#inline-popups-24,#inline-popups-25,#inline-popups-26,#inline-popups-27,#inline-popups-28,#inline-popups-29,#inline-popups-30,#inline-popups-31,#inline-popups-32,#inline-popups-33,#inline-popups-34,#inline-popups-35,#inline-popups-36,#inline-popups-37,#inline-popups-38,#inline-popups-39,#inline-popups-40,#inline-popups-41,#inline-popups-42,#inline-popups-43,#inline-popups-44,#inline-popups-45,#inline-popups-46,#inline-popups-47,#inline-popups-48,#inline-popups-49,#inline-popups-50,#inline-popups-51,#inline-popups-52,#inline-popups-53,#inline-popups-54,#inline-popups-55,#inline-popups-56,#inline-popups-57').magnificPopup({
-  delegate: 'a',
-  removalDelay: 500, //delay removal by X to allow out-animation
-  callbacks: {
-    beforeOpen: function() {
-       this.st.mainClass = this.st.el.attr('data-effect');
-    }
-  },
-  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-
-
-  
-
-
+$(function() {
+ $('#sdt_menu > li').bind('mouseenter',function(){
+  var $elem = $(this);
+  $elem.find('img')
+    .stop(true)
+    .animate({
+    'width':'170px',
+    'height':'170px',
+    'left':'0px'
+    },400,'easeOutBack')
+    .andSelf()
+    .find('.sdt_wrap')
+    .stop(true)
+    .animate({'top':'140px'},500,'easeOutBack')
+    .andSelf()
+    .find('.sdt_active')
+    .stop(true)
+    .animate({'height':'170px'},300,function(){
+   var $sub_menu = $elem.find('.sdt_box');
+   if($sub_menu.length){
+    var left = '170px';
+    if($elem.parent().children().length == $elem.index()+1)
+     left = '-170px';
+    $sub_menu.show().animate({'left':left},200);
+   }
+  });
+ }).bind('mouseleave',function(){
+  var $elem = $(this);
+  var $sub_menu = $elem.find('.sdt_box');
+  if($sub_menu.length)
+   $sub_menu.hide().css('left','0px');
+ 
+  $elem.find('.sdt_active')
+    .stop(true)
+    .animate({'height':'0px'},300)
+    .andSelf().find('img')
+    .stop(true)
+    .animate({
+    'width':'0px',
+    'height':'0px',
+    'left':'85px'},400)
+    .andSelf()
+    .find('.sdt_wrap')
+    .stop(true)
+    .animate({'top':'25px'},500);
+ });
 });
 
 
