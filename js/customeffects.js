efa2051ab10a355729471e2836c398bda602f431
br1anp4rk <br1anp4rk718@gmail.com>
2016-02-17 20:36:49 -0800

    carousel with timer added

--- js/customeffects.js
+++ js/customeffects.js
@@ -21,15 +21,25 @@ $(document).ready(function() {
 
 	});
 
-
-	$('#sdt_menu li').on('mouseover', function() {
-
-		$('.compoundinfo').css('margin-top', '15em');
+	$('#sdt_menu li').hover(function() {
+		$('.compoundinfo').animate({
+			'margin-top':'15em'
+		}, 500)
+	}, function() {
+		$('.compoundinfo').css('margin-top', 'initial').animate({
+			'margin-top':'-3em'
+		}, 200)
 	});
 
-	$('#sdt_menu li').on('mouseleave', function() {
 
-		$('.compoundinfo').css('margin-top', 'initial');
-	});
+	// $('#sdt_menu li').on('mouseover', function() {
+
+	// 	$('.compoundinfo').animate({'margin-top':'15em'}, 500);
+	// });
+
+	// $('#sdt_menu li').on('mouseleave', function() {
+
+	// 	$('.compoundinfo').animate({'margin-top':'initial'}, 500);
+	// });
 
 })
