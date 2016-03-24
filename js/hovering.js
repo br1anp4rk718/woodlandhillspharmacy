0d49642f22affc302fe66c9b3be26775fe2b5df5
br1anp4rk <br1anp4rk718@gmail.com>
2016-03-23 20:51:14 -0700

    js file hovering to compounding accordians

--- js/hovering.js
+++ js/hovering.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	$('.panel-title').on('mouseenter', function() {
 
 		$(this).find('a').css('color', '#009bdb');
+		$(this).
 	});
 
 	$('.panel-title').on('mouseleave', function() {
