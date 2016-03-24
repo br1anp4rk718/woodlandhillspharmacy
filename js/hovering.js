4759714dbde3c9450b43f91f8d69bc646261e6ad
br1anp4rk <br1anp4rk718@gmail.com>
2016-03-23 21:01:35 -0700

    hovering function on accordian pages

--- js/hovering.js
+++ js/hovering.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
 	$('.panel-title').on('mouseenter', function() {
 
 		$(this).find('a').css('color', '#009bdb');
-		$(this).
+		$(this).closest('.panel-heading').css('background-color', 'rgba(0,155,219,0.5)');
+		debugger;
+
 	});
 
 	$('.panel-title').on('mouseleave', function() {
 
 		$(this).find('a').css('color', 'black');
+		$(this).closest('.panel-heading').css('background-color', '#f1f1f1');
 	});
+
+
 })
